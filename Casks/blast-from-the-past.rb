22275aea94ea21f4f4e710f9188e7bdbd8a27919
Ravin Bhakta <ravin.bhakta@gmail.com>
2026-04-10 15:14:07 -0700

    Update SHA256 checksums for v1.2.6 DMG files

--- Casks/blast-from-the-past.rb
+++ Casks/blast-from-the-past.rb
@@ -3,12 +3,12 @@ cask "blast-from-the-past" do
 
   on_intel do
     url "https://github.com/bhaktaravin/blast-from-the-past-messenger/releases/download/v#{version}/blast-from-the-past-macos-intel.dmg"
-    sha256 "99b4ea961798dc5335b44f18093e90a0b4304b62d99566726b97bafd4ca853c1"
+    sha256 "36252af7ec083e7c8c771f1b1f4f434fe402bc0bf7f4d626fd950022840d3a24"
   end
 
   on_arm do
     url "https://github.com/bhaktaravin/blast-from-the-past-messenger/releases/download/v#{version}/blast-from-the-past-macos-silicon.dmg"
-    sha256 "to_be_updated"
+    sha256 "89636799cd41d2897ab1bfca144e539f6ef021fb2b4128409dd9c6b60652a42a"
   end
 
   name "Blast From The Past"
